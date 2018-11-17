096cc423b7c61713e47dffa0f9bb5c61a2c1c672
Soo J. Cho <beantownbuilder@gmail.com>
2018-11-17 03:19:40 -0500

    add form for messages and jQuery and incoming message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -3,25 +3,27 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server');
-
-  // socket.emit('createEmail',{
-  //   to: 'soo@example.com',
-  //   text: 'hey this is soo'
-  // });
-
-
 });
 
 socket.on('disconnect', function () {
   console.log('disconnected from server');
 });
-
-// //new custom event "newEmail"
-// socket.on('newEmail', function (email){
-//   console.log('new email', email);
-// });
-
 //print message from server in client
 socket.on('newMessage', function (message){
   console.log('new message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(){
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -27,9 +27,10 @@ io.on('connection',(socket)=>{
   socket.broadcast.emit('newMessage', generateMessage('Admin','new user joined'));
 
 //prints message created from client in cmd/server
-  socket.on('createMessage',(message) =>{
+  socket.on('createMessage',(message, callback) =>{
     console.log('create Message',message);
     io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('this is from the server');//ack
     // socket.broadcast.emit('newMessage',{
     //   from: message.from,
     //   text: message.text,
